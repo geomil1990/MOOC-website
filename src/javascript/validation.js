7eb0a557444b5f356ce66c1dde9584c1ac5099ba
GeoMil <geo_mil@windowslive.com>
2021-12-06 21:29:25 +0200

    simple check for age done

--- src/javascript/validation.js
+++ src/javascript/validation.js
@@ -1,4 +1,4 @@
-const validateRegistrationForm = evt => {
+/*const validateRegistrationForm = evt => {
     evt.preventDefault();
 
     // Get the error message divs
@@ -14,4 +14,11 @@ const validateRegistrationForm = evt => {
 
     // Redirect to the homepage
     window.location.href.replace('http://127.0.0.1:5501/src/html/index.html')
+}*/
+function checkforAge() {
+  if (document.getElementById("age").value < 18) {
+    alert("You should be over 18 to register");
+    document.getElementById("age").style.borderColor = "red";
+    return false;
+  }
 }
