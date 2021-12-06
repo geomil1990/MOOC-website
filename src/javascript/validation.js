3d40cc098cc268776e3479675f06243275a547e5
KonstantinosVasilopoulos <costasvas34@protonmail.com>
2021-12-06 19:15:57 +0200

    Validation JavaScript added

---
+++ src/javascript/validation.js
@@ -0,0 +1,17 @@
+const validateRegistrationForm = evt => {
+    evt.preventDefault();
+
+    // Get the error message divs
+    const ageError = document.getElementById('register-error-age')
+    const passwordError = document.getElementById('register-error-password')
+
+    // Ensure that all new users are above 18 years old
+    const ageInput = document.getElementById('age')
+    if (ageInput.value < 18) {
+        ageError.innerText = 'Only users above 18 years old are allowed to register!'
+        return
+    }
+
+    // Redirect to the homepage
+    window.location.href.replace('http://127.0.0.1:5501/src/html/index.html')
+}
